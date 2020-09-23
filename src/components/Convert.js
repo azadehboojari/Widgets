e29793e3a4dddc80f5ba5e68affea77989f03ba3
azadehboojari <azadeh.bojari@yahoo.com>
2020-09-22 23:46:50 -0700

    Google Translate API  added to convert Widgets

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,14 +1,40 @@
 // https://translation.googleapis.com/language/translate/v2
 // AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropdown from './Dropdown';
-import Translate from './Translate'
+import Translate from './Translate';
+import axios from 'axios';
 
-const Convert= (language, text)=>{
+const Convert = ({ language, text }) => {
+    const [translated, setTreanslated] = useState('')
+    const [debouncedText, setDebouncedText] = useState(text)
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text)
+        }, 700)
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [text])
+
+    useEffect(() => {
+        const doTransation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: debouncedText,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            })
+            setTreanslated(data.data.translations[0].translatedText)
+        }
+
+        doTransation()
+    }, [language, debouncedText])
 
     return (
         <div>
-            Convert
+            <h1 className="ui header">{translated}</h1>
         </div>
     )
 }
